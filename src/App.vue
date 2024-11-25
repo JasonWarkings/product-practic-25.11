2fe21b7f6beddd4ae936afdf590ad2ec93a5e3f0
Одорский Доминик <dominik10072007@gmail.com>
2024-11-25 11:13:05 +0500

    feat: add list products

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,35 @@
 <script setup>
-
+import { ref } from "vue";
+const products = ref([
+  {
+    id: Date.now(),
+    name: 'Tesla',
+    date: '20.11.2024',
+    count: 10,
+    price: 4000,
+  },
+  {
+    id: Date.now(),
+    name: 'Aser',
+    date: '10.11.2024',
+    count: 150,
+    price: 1000,
+  },
+  {
+    id: Date.now(),
+    name: 'HP',
+    date: '11.11.2024',
+    count: 18,
+    price: 5000,
+  },
+  {
+    id: Date.now(),
+    name: 'Asus',
+    date: '05.11.2024',
+    count: 25,
+    price: 3000,
+  }
+])
 </script>
 
 <template>
@@ -29,13 +59,13 @@
       </div>
     </div>
     <div class="row row-cols-1 row-cols-md-2 mt-4">
-  <div class="col">
+  <div class="col" v-for="product in products">
     <div class="card">
       <div class="card-body">
-        <h5 class="card-title">HP</h5>
-        <p class="card-text">$1000</p>
-        <p class="card-text">x10</p>
-        <p class="card-text">25.11.2024</p>
+        <h5 class="card-title">{{ product.name }}</h5>
+        <p class="card-text">{{ product.price }}</p>
+        <p class="card-text">{{ product.count }}</p>
+        <p class="card-text">{{ product.date }}</p>
       </div>
       <div class="card-footer">
         <button class="btn btn-outline-danger">Удалить</button>
